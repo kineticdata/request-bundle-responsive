f5502e82d418dd0cbd3d5f89c00412ff75eb11d5
croucha <andre.crouch@kineticdata.com>
2014-09-14 16:42:55 -0500

    refactored js inside catalog

--- theme/packages/catalog/resources/js/catalog.js
+++ theme/packages/catalog/resources/js/catalog.js
@@ -1,85 +1,85 @@
-$(function() {
-    $(window).hashchange(function(event) {
-        console.log(window.location.hash.substring(1))
-    });
-    // Set some states
-    $('ul li.catalog').addClass('active').append($('<div>').addClass('arrow-left'));
-    $('ul.categories').append($('ul.root-category-data').html());
+(function($, _) {
+    /*----------------------------------------------------------------------------------------------
+     * DOM MANIPULATION AND EVENT REGISTRATION
+     *   This section is executed on page load to register events and otherwise manipulate the DOM.
+     *--------------------------------------------------------------------------------------------*/
+    $(function() {
+        // Set some states
+        $('ul li.catalog').addClass('active').append($('<div>').addClass('arrow-left'));
+        $('ul.categories').append($('ul.root-category-data').html());
 
-    // Selectors that are used quite a bit
-    var breadcrumbs = 'nav.catalog ul.breadcrumbs';
-    var templates = 'nav.catalog ul.templates';
-    var templateDetails = 'nav.catalog div.template-details';
-    var categories = 'nav.catalog ul.categories';
-    
-    // Click event for menus
-    $(categories).on('click', 'li', function(event) {
-        event.preventDefault();
-        // Get current clicked category id and name
-        var categoryId =  $(this).data('id');
-        var categoryName =  $(this).data('name');
-        // Find category's children and get the html
-        var currentSubcategories = $('ul.category-data li[data-id=' + categoryId + ']').find('ul.subcategory-data').html();
-        // Display the category's children if not undefined
-        currentSubcategories !== undefined ? $(categories).html(currentSubcategories) : $(categories).empty();
-        // Find category's templates and get the html
-        var currentTemplates = $('ul.category-data li[data-id=' + categoryId + ']').find('ul.template-data').html();
-        // Display the category's templates into div
-        currentTemplates !== undefined ? $(templates).html(currentTemplates) : $(templates).empty();
-        // Create new bread crumb with the category's information
-        $(breadcrumbs).append(
-            $('<li>').attr('data-id', $(this).data('id'))
-                .attr('data-name', $(this).data('name'))
-                .append($(this).data('name'))
-                .append(
-                    $('<i>').addClass('fa fa-chevron-circle-left')
-                )
-        ).show();
-        // Update hash
-        window.location.hash = categoryName;
-    });
-    // Click event for template details
-    $(templates).on('click', 'li', function(event) {
-        event.preventDefault();
-        var templateName =  $(this).data('name');
-        // Clean up
-        $(templates).empty();
-        $(categories).empty();
-        // Show details
-        $(templateDetails).html($(this).find('div.template-details-data').html());
-        // Create new bread crumb with the template's information
-        $(breadcrumbs).append(
-            $('<li>').attr('data-id', $(this).data('id'))
-                .attr('data-name', $(this).data('name'))
-                .append($(this).data('name'))
-                .append(
-                    $('<i>').addClass('fa fa-chevron-circle-left')
-                )
-        ).show();
-    });
-    
-    // Click event for breadcrumbs
-    $(breadcrumbs).on('click', 'li', function(event) {
-        event.preventDefault();
-        $(templateDetails).empty();
-        // Get previous id
-        var previousCategoryId = $(this).prev().data('id');
-        // Check if root bread crumb is selected or prev undefined
-        if(previousCategoryId === 'root' || previousCategoryId === undefined) {
-            $(categories).html($('ul.root-category-data').html());
-            $(templates).empty();
-            if(previousCategoryId !== undefined) {
-                $(this).remove();
-            }  
-        } else {
-            var currentSubcategories = $('ul.category-data li[data-id=' + previousCategoryId + ']').find('ul.subcategory-data').html();
+        // Selectors that are used quite a bit
+        var breadcrumbs = 'nav.catalog ul.breadcrumbs';
+        var templates = 'nav.catalog ul.templates';
+        var templateDetails = 'nav.catalog div.template-details';
+        var categories = 'nav.catalog ul.categories';
+
+        // Click event for menus
+        $(categories).on('click', 'li', function(event) {
+            event.preventDefault();
+            // Get current clicked category id and name
+            var categoryId =  $(this).data('id');
+            var categoryName =  $(this).data('name');
+            // Find category's children and get the html
+            var currentSubcategories = $('ul.category-data li[data-id=' + categoryId + ']').find('ul.subcategory-data').html();
             // Display the category's children if not undefined
             currentSubcategories !== undefined ? $(categories).html(currentSubcategories) : $(categories).empty();
-            var currentTemplates = $('ul.category-data li[data-id=' + previousCategoryId + ']').find('ul.template-data').html();
+            // Find category's templates and get the html
+            var currentTemplates = $('ul.category-data li[data-id=' + categoryId + ']').find('ul.template-data').html();
             // Display the category's templates into div
             currentTemplates !== undefined ? $(templates).html(currentTemplates) : $(templates).empty();
-            // Remove current breadcrumb
-            $(this).remove();
-        }
+            // Create new bread crumb with the category's information
+            $(breadcrumbs).append(
+                $('<li>').attr('data-id', $(this).data('id'))
+                    .attr('data-name', $(this).data('name'))
+                    .append($(this).data('name'))
+                    .append(
+                        $('<i>').addClass('fa fa-chevron-circle-left')
+                    )
+            ).show();
+        });
+        // Click event for template details
+        $(templates).on('click', 'li', function(event) {
+            event.preventDefault();
+            // Clean up
+            $(templates).empty();
+            $(categories).empty();
+            // Show details
+            $(templateDetails).html($(this).find('div.template-details-data').html());
+            // Create new bread crumb with the template's information
+            $(breadcrumbs).append(
+                $('<li>').attr('data-id', $(this).data('id'))
+                    .attr('data-name', $(this).data('name'))
+                    .append($(this).data('name'))
+                    .append(
+                        $('<i>').addClass('fa fa-chevron-circle-left')
+                    )
+            ).show();
+        });
+
+        // Click event for breadcrumbs
+        $(breadcrumbs).on('click', 'li', function(event) {
+            event.preventDefault();
+            $(templateDetails).empty();
+            // Get previous id
+            var previousCategoryId = $(this).prev().data('id');
+            // Check if root bread crumb is selected or prev undefined
+            if(previousCategoryId === 'root' || previousCategoryId === undefined) {
+                $(categories).html($('ul.root-category-data').html());
+                $(templates).empty();
+                if(previousCategoryId !== undefined) {
+                    $(this).remove();
+                }  
+            } else {
+                var currentSubcategories = $('ul.category-data li[data-id=' + previousCategoryId + ']').find('ul.subcategory-data').html();
+                // Display the category's children if not undefined
+                currentSubcategories !== undefined ? $(categories).html(currentSubcategories) : $(categories).empty();
+                var currentTemplates = $('ul.category-data li[data-id=' + previousCategoryId + ']').find('ul.template-data').html();
+                // Display the category's templates into div
+                currentTemplates !== undefined ? $(templates).html(currentTemplates) : $(templates).empty();
+                // Remove current breadcrumb
+                $(this).remove();
+            }
+        });
     });
-});
+})(jQuery, _);
--- theme/packages/catalog/resources/js/package.js
+++ theme/packages/catalog/resources/js/package.js
@@ -1,80 +1,106 @@
-$(function() {
-    dropDown();
-});
-
-function dropDown() {
-    var firstHover = true;
-    $('ul.dropdown-menus').on({
-        mouseenter: function() {
-            if (firstHover) {
-                firstHover = false;
-            }
-        }
-    }, 'li.category');
-
-    $('nav ul.nav li.dropdown').on({
-        mouseenter: function() {
-            // Show flyout table with root cats
-            $(this).find('table.flyout-table').show();
-            $(this).addClass('nav-hover');
-            $(this).find('i.nav-sprite').css({'background-position':'-127px -22px'});
-        },
-        mouseleave: function() {
-            // Reset first hover to true so animation will work again
-            firstHover = true; 
-            // Reset flyout
-            $(this).find('.flyout').width('250px');
-            $('.subcats-wrap').hide();
-            $(this).find('.flyout-table').addClass('exposed-skin');
-            // Reset drop down and dropdown flyout table
-            $('nav ul.nav li.dropdown').removeClass('nav-hover');
-            $('i.nav-sprite').css({'background-position':'-127px -12px'});
-            $(this).find('table.flyout-table').hide();
-            $('.subcats-wrap').hide();
-            $(this).find('.flyout-table').addClass('exposed-skin');
-        }
+(function($, _) {
+    /*----------------------------------------------------------------------------------------------
+     * DOM MANIPULATION AND EVENT REGISTRATION
+     *   This section is executed on page load to register events and otherwise manipulate the DOM.
+     *--------------------------------------------------------------------------------------------*/
+    $(function() {
+        package.dropDown();
     });
-}
-
-function flyout() {
-    var firstHover = true;
-    $('ul.dropdown-menus').on({
-        mouseenter: function() {
-            if (firstHover) {
-                firstHover = false;
-                // Animate flyout
-                $('.flyout').animate({ width: '640px' });
-            } else {
-                // Show flyout
-                $('.flyout').width('640px');
+    
+    /*----------------------------------------------------------------------------------------------
+     * PACKAGE INIALIZATION
+     *   This code is executed when the Javascript file is loaded
+     *--------------------------------------------------------------------------------------------*/
+    
+    // Ensure the BUNDLE global object exists
+    BUNDLE = BUNDLE || {};
+    // Create the package namespace
+    BUNDLE.package = BUNDLE.package || {};
+    // Create a scoped alias to simplify references
+    var package = BUNDLE.package;
+    
+    /**
+     * Initializes the dropdown menus
+     * @returns {undefined}
+     */
+    package.dropDown = function() {
+        var firstHover = true;
+        $('ul.dropdown-menus').on({
+            mouseenter: function() {
+                if (firstHover) {
+                    firstHover = false;
+                }
             }
-            $('.subcats-wrap').show();
-            $('.subcats').empty().html($(this).find('ul.subcategories').html()).show();
-            $('.subcats').append($(this).find('.category-image').html());
-            $('.flyout-table').removeClass('exposed-skin');
-        }
-    }, 'li.category');
+        }, 'li.category');
 
-    $('nav ul.nav li.dropdown').on({
-        mouseenter: function() {
-            // Show flyout table with root cats
-            $('table.flyout-table').show();
-            $(this).addClass('nav-hover');
-            $(this).find('i.nav-sprite').css({'background-position':'-127px -22px'});
-        },
-        mouseleave: function() {
-            // Reset first hover to true so animation will work again
-            firstHover = true; 
-            // Reset flyout
-            $('.flyout').width('250px' );
-            $('.subcats-wrap').hide();
-            $('.flyout-table').addClass('exposed-skin');
-            // Reset drop down and dropdown flyout table
-            $('nav ul.nav li.dropdown').removeClass('nav-hover');
-            $('i.nav-sprite').css({'background-position':'-127px -12px'});
-            $('table.flyout-table').hide();
-            $('.subcats-wrap').hide();
-            $('.flyout-table').addClass('exposed-skin');
-        }
-    });
-}
+        $('nav ul.nav li.dropdown').on({
+            mouseenter: function() {
+                // Show flyout table with root cats
+                $(this).find('table.flyout-table').show();
+                $(this).addClass('nav-hover');
+                $(this).find('i.nav-sprite').css({'background-position':'-127px -22px'});
+            },
+            mouseleave: function() {
+                // Reset first hover to true so animation will work again
+                firstHover = true; 
+                // Reset flyout
+                $(this).find('.flyout').width('250px');
+                $('.subcats-wrap').hide();
+                $(this).find('.flyout-table').addClass('exposed-skin');
+                // Reset drop down and dropdown flyout table
+                $('nav ul.nav li.dropdown').removeClass('nav-hover');
+                $('i.nav-sprite').css({'background-position':'-127px -12px'});
+                $(this).find('table.flyout-table').hide();
+                $('.subcats-wrap').hide();
+                $(this).find('.flyout-table').addClass('exposed-skin');
+            }
+        });
+    };
+    
+    /**
+     * Initializes the flyout menus
+     * @returns {undefined}
+     */
+    package.flyout = function() {
+        var firstHover = true;
+        $('ul.dropdown-menus').on({
+            mouseenter: function() {
+                if (firstHover) {
+                    firstHover = false;
+                    // Animate flyout
+                    $('.flyout').animate({ width: '640px' });
+                } else {
+                    // Show flyout
+                    $('.flyout').width('640px');
+                }
+                $('.subcats-wrap').show();
+                $('.subcats').empty().html($(this).find('ul.subcategories').html()).show();
+                $('.subcats').append($(this).find('.category-image').html());
+                $('.flyout-table').removeClass('exposed-skin');
+            }
+        }, 'li.category');
+
+        $('nav ul.nav li.dropdown').on({
+            mouseenter: function() {
+                // Show flyout table with root cats
+                $('table.flyout-table').show();
+                $(this).addClass('nav-hover');
+                $(this).find('i.nav-sprite').css({'background-position':'-127px -22px'});
+            },
+            mouseleave: function() {
+                // Reset first hover to true so animation will work again
+                firstHover = true; 
+                // Reset flyout
+                $('.flyout').width('250px' );
+                $('.subcats-wrap').hide();
+                $('.flyout-table').addClass('exposed-skin');
+                // Reset drop down and dropdown flyout table
+                $('nav ul.nav li.dropdown').removeClass('nav-hover');
+                $('i.nav-sprite').css({'background-position':'-127px -12px'});
+                $('table.flyout-table').hide();
+                $('.subcats-wrap').hide();
+                $('.flyout-table').addClass('exposed-skin');
+            }
+        });
+    };
+})(jQuery, _);
