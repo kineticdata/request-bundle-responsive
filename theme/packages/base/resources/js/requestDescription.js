303a566adfc86c2db735c2783597b41140e3d3ca
croucha <andre.crouch@kineticdata.com>
2014-09-14 17:13:12 -0500

    refactored base js

--- theme/packages/base/resources/js/requestDescription.js
+++ theme/packages/base/resources/js/requestDescription.js
@@ -1,7 +1,13 @@
-$(document).ready(function() {
-    if($('[label="Description Right"]').exists()) {
-        $('[label="Description Right"]').append($('a.create-request').show('fade', 1000));
-    } else {
-        $('a.create-request').show('fade', 1000);
-    }
-});
+(function($, _) {
+    /*----------------------------------------------------------------------------------------------
+     * DOM MANIPULATION AND EVENT REGISTRATION
+     *   This section is executed on page load to register events and otherwise manipulate the DOM.
+     *--------------------------------------------------------------------------------------------*/
+    $(function() {
+        if($('[label="Description Right"]').exists()) {
+            $('[label="Description Right"]').append($('a.create-request').show('fade', 1000));
+        } else {
+            $('a.create-request').show('fade', 1000);
+        }
+    });
+})(jQuery, _);
