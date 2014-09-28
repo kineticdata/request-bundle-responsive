2171c306a26728daa48b0338e86328fb3e2019e2
croucha <andre.crouch@kineticdata.com>
2014-09-28 10:21:08 -0500

    removed method that are replaced by underscore and client js

--- common/resources/js/common.js
+++ common/resources/js/common.js
@@ -183,38 +183,6 @@
     };
     
     /**
-     * 
-     * @deprecated underscoreJS has the method now.
-     * @param {Integer} start
-     * @param {Integer} end
-     * @returns {Array}
-     */
-    common.range = function(start, end) {
-        var array = new Array();
-        for(var i = start; i <= end; i++) {
-            array.push(i);
-        }
-        return array;
-    };
-    
-    /**
-     * Returns the version of Internet Explorer or a -1 (indicating the use of another browser).
-     * From: http://msdn.microsoft.com/en-us/library/ms537509(v=vs.85).aspx
-     * @returns {Number}
-     */
-    common.getInternetExplorerVersion = function() {
-        var rv = -1; // Return value assumes failure.
-        if (navigator.appName === 'Microsoft Internet Explorer') {
-            var ua = navigator.userAgent;
-            var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
-            if (re.exec(ua) !== null) {
-                rv = parseFloat( RegExp.$1 );
-            }
-        }
-        return rv;
-    };
-    
-    /**
      * Live jQuery hover function used to display specific behavior when a user hovers over dhildren selector.
      * The parent slector of the children are quired for the even to be live.
      * 
