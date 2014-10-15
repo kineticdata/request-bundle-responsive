2222b056e18df22e9f8b6c5333b37a1afee0a458
croucha <andre.crouch@kineticdata.com>
2014-10-14 20:38:30 -0500

    updating configuration for callbacks

--- packages/submissions/resources/js/submissionsList.js
+++ packages/submissions/resources/js/submissionsList.js
@@ -251,7 +251,7 @@
                     dataType: 'json',
                     cache: false,
                     type: 'get',
-                    url: BUNDLE.config.submissionsUrl + '&view=submissionsJson&qualification=' + status + '&offset=' + index + '&limit=' + limit + '&orderField=' + sortOrderField + '&order=' + sortOrder,
+                    url: BUNDLE.config.submissionsUrl + '&callback=submissionsJson&qualification=' + status + '&offset=' + index + '&limit=' + limit + '&orderField=' + sortOrderField + '&order=' + sortOrder,
                     beforeSend: function(jqXHR, settings) {
                         responseMessage.empty();
                         loader.show();
